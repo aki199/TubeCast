b1aef11db4e083426fd643ae48991535d945be58
akmalu21 <akmalu21@gmail.com>
2017-03-25 20:57:38 +0000

    Created pages controller. Added Explore, home, index and profile pages. Changed Root.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+# The pages controller contains all of the code for any page inside of /pages
+class PagesController < ApplicationController
+  
+  # back-end code for pages/index
+  def index
+  end
+
+# back-ned code for pages/home
+  def home
+  end
+# back-ned code for pages/profile
+
+  def profile
+  end
+# back-ned code for pages/eplore
+
+  def eplore
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+ 
+ # Define Root URL
+ root 'pages#index'
+ # Define routes for Pages
+ 
+
+  get 'home' => 'pages#home'
+
+  get 'profile' => 'pages#profile'
+
+  get 'eplore' => 'pages#eplore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
